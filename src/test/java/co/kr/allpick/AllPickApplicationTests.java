754c0901d457899b4698dc0bd815e6d59aad6be1
silvery <dmsquf9193@gmail.com>
2026-04-14 18:07:07 +0900

    chore: 프로젝트 패키지 구조 초기 세팅

---
+++ src/test/java/co/kr/allpick/AllPickApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.allpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
