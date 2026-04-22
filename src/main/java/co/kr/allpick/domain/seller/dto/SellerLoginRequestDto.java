eb6afd9bdf30ca1e3a997640c26c57e2515e6e70
Hello-pythworld <yeonghun4051@daum.net>
2026-04-22 14:35:45 +0900

    feat : seller쪽 코드 삭제

--- src/main/java/co/kr/allpick/domain/seller/dto/SellerLoginRequestDto.java
+++ src/main/java/co/kr/allpick/domain/seller/dto/SellerLoginRequestDto.java
@@ -1,14 +1,5 @@
 package co.kr.allpick.domain.seller.dto;
 
-import jakarta.validation.constraints.*;
-import lombok.Getter;
-
-@Getter
 public class SellerLoginRequestDto {
 
-    @NotBlank @Email(message = "이메일 형식이 올바르지 않습니다.")
-    private String email;
-
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    private String password;
 }
--- src/main/java/co/kr/allpick/domain/seller/dto/SellerUpdateRequestDto.java
+++ src/main/java/co/kr/allpick/domain/seller/dto/SellerUpdateRequestDto.java
@@ -1,20 +1,5 @@
 package co.kr.allpick.domain.seller.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-
-@Getter
 public class SellerUpdateRequestDto {
 
-    @NotBlank(message = "이름을 입력해주세요.")
-    private String name;
-
-    @NotBlank(message = "전화번호를 입력해주세요.")
-    private String phone;
-
-    @NotBlank(message = "주소를 입력해주세요.")
-    private String address;
-
-    @NotBlank(message = "상호명을 입력해주세요.")
-    private String businessName;
 }
