bc232030f672f37bfd0b7549940396278e76fa0a
yul941117-wq <yul941117@gamil.com>
2026-04-24 17:25:07 +0900

    refactor: import * 제거

--- src/main/java/co/kr/allpick/domain/product/entity/Product.java
+++ src/main/java/co/kr/allpick/domain/product/entity/Product.java
@@ -1,7 +1,15 @@
 package co.kr.allpick.domain.product.entity;
-
 import co.kr.allpick.global.common.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
