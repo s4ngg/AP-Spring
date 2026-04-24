e822f6cf445ef946df662a4b34b3695f0d393473
sbr7518-bit <sbr7518@gmail.com>
2026-04-24 16:01:12 +0900

    ìfix: InquiryControllerDocs impot 삭제

--- src/main/java/co/kr/allpick/domain/admin/product/controller/docs/InquiryControllerDocs.java
+++ src/main/java/co/kr/allpick/domain/admin/product/controller/docs/InquiryControllerDocs.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Tag(name = "Inquiry", description = "문의 API")
