ad4a8cb425e72b1266ff67fd93ed0eead4d0c426
beanCan <bean.can0325@gmail.com>
2026-04-24 15:54:45 +0900

    feat: AdminServiceImpl 구현

--- src/main/java/co/kr/allpick/domain/admin/service/AdminServiceImpl.java
+++ src/main/java/co/kr/allpick/domain/admin/service/AdminServiceImpl.java
@@ -1,12 +1,84 @@
 package co.kr.allpick.domain.admin.service;
 
 
+import co.kr.allpick.domain.admin.dto.AdminCreateRequestDto;
+import co.kr.allpick.domain.admin.dto.AdminLoginRequestDto;
+import co.kr.allpick.domain.admin.dto.AdminLoginResponseDto;
+import co.kr.allpick.domain.admin.entity.Admin;
 import co.kr.allpick.domain.admin.repository.AdminRepository;
+import co.kr.allpick.global.config.JwtProvider;
+import co.kr.allpick.global.config.JwtUserInfoDto;
+import co.kr.allpick.global.exception.BusinessException;
+import co.kr.allpick.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class AdminServiceImpl implements AdminRepository {
+public class AdminServiceImpl implements AdminService {
 
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public AdminLoginResponseDto adminLogin(AdminLoginRequestDto adminLoginRequestDto) {
+
+
+        Admin admin =
+                adminRepository.findByEmail(adminLoginRequestDto.getEmail())
+                        .orElseThrow(() -> new
+                                BusinessException(ErrorCode.ADMIN_NOT_FOUND));
+
+        if (!passwordEncoder.matches(adminLoginRequestDto.getPassword(), admin.getPassword())) {
+            throw new BusinessException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        JwtUserInfoDto jwtUserInfoDto = new JwtUserInfoDto(
+                admin.getAdminId(),
+                admin.getEmail(),
+                admin.getRole().name()
+        );
+
+        String token = jwtProvider.createToken(jwtUserInfoDto);
+
+
+        return AdminLoginResponseDto.builder()
+                .adminName(admin.getAdminName())
+                .role(admin.getRole().name())
+                .token(token)
+                .build();
+
+    }
+
+    @Override
+    public void createAdmin(AdminCreateRequestDto adminCreateRequestDto) {
+
+        //1. 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(adminCreateRequestDto.getPassword());
+
+        //2. Admin 객체 생성
+        Admin admin = Admin.builder()
+                .adminName(adminCreateRequestDto.getAdminName())
+                .email(adminCreateRequestDto.getEmail())
+                .password(encodedPassword) //암호화된 비밀번호 저장
+                .adminPhone(adminCreateRequestDto.getAdminPhone())
+                .role(Admin.AdminRole.valueOf(adminCreateRequestDto.getRole()))
+                .build();
+
+        adminRepository.save(admin);
+
+
+    }
+
+    @Override
+    public void updateStatus(Long adminId, Admin.AdminStatus status) {
+
+        Admin admin = adminRepository.findById(adminId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ADMIN_NOT_FOUND));
+        admin.updateStatus(status);
+    }
 }
