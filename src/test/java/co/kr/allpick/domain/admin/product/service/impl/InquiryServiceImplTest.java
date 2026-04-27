7af10ed82a00bd84e98ef8ee231fb7ffa2076bd4
Bora <sbr7518@gmail.com>
2026-04-27 11:21:32 +0900

    fix: InquiryServiceImplTest 누락 Mock 추가 - MemberRepository, OrderItemRepository @Mock 추가 - 문의 등록 성공 테스트에 existsById stub 추가

--- src/test/java/co/kr/allpick/domain/admin/product/service/impl/InquiryServiceImplTest.java
+++ src/test/java/co/kr/allpick/domain/admin/product/service/impl/InquiryServiceImplTest.java
@@ -8,6 +8,8 @@ import co.kr.allpick.domain.admin.product.entity.Inquiry;
 import co.kr.allpick.domain.admin.product.entity.InquiryAnswer;
 import co.kr.allpick.domain.admin.product.repository.InquiryAnswerRepository;
 import co.kr.allpick.domain.admin.product.repository.InquiryRepository;
+import co.kr.allpick.domain.member.repository.MemberRepository;
+import co.kr.allpick.domain.order.repository.OrderItemRepository;
 import co.kr.allpick.global.exception.BusinessException;
 import co.kr.allpick.global.exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
@@ -34,6 +36,12 @@ class InquiryServiceImplTest {
     @Mock
     InquiryAnswerRepository inquiryAnswerRepository;
 
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    OrderItemRepository orderItemRepository;
+
     @InjectMocks
     InquiryServiceImpl inquiryService;
 
@@ -54,6 +62,8 @@ class InquiryServiceImplTest {
                 .content("정 사이즈인지 궁금합니다.")
                 .build();
 
+        when(memberRepository.existsById(1L)).thenReturn(true);
+        when(orderItemRepository.existsById(10L)).thenReturn(true);
         when(inquiryRepository.save(any(Inquiry.class))).thenReturn(mockInquiry);
 
         // when
