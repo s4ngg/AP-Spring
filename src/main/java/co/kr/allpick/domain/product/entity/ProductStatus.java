2fbff8b7c370b73636d3695de5aabacc18da194b
piano <gungang1212@gmail.com>
2026-04-27 14:10:59 +0900

    원격 파일 삭제(product)

--- src/main/java/co/kr/allpick/domain/product/entity/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package co.kr.allpick.domain.product.entity;
-
-public enum ProductStatus {
-    ACTIVE,
-    INACTIVE
-}
--- src/main/java/co/kr/allpick/domain/product/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package co.kr.allpick.domain.product.service;
-
-import co.kr.allpick.domain.product.dto.ProductDetailResDto;
-
-public interface ProductService {
-	// 상품 id로 상품상세 페이지 조회하기
-	ProductDetailResDto getProductDetail(Long productId);
-}
