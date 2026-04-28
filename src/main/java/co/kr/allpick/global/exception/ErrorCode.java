7d4a8613c1d69c559e932923db354d83eaf2774e
Bora <sbr7518@gmail.com>
2026-04-28 12:15:23 +0900

    fix:ErrorCode 오류 해결

--- src/main/java/co/kr/allpick/global/exception/ErrorCode.java
+++ src/main/java/co/kr/allpick/global/exception/ErrorCode.java
@@ -43,7 +43,7 @@ public enum ErrorCode {
 
     //Admin
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "ADMIN_NOT_FOUND", "관리자를 찾을 수 없습니다."),
-    ADMIN_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "ADMIN_DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다.");
+    ADMIN_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "ADMIN_DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다."),
 
 	
     // Product
