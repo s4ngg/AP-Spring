bca58b8d4aa04c59c3463ca903d2a0bedb50360c
Bora <sbr7518@gmail.com>
2026-04-28 18:00:10 +0900

    feat:상품 목록 페이지 조회 API 구현

--- src/main/java/co/kr/allpick/domain/product/controller/ProductController.java
+++ src/main/java/co/kr/allpick/domain/product/controller/ProductController.java
@@ -1,5 +1,10 @@
 package co.kr.allpick.domain.product.controller;
 
+import co.kr.allpick.domain.product.dto.ProductListResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +24,13 @@ import lombok.RequiredArgsConstructor;
 public class ProductController implements ProductControllerDocs{
 
 	private final ProductService productService;
+
+	@Override
+	@GetMapping
+	public ResponseEntity<ApiResponse<Page<ProductListResponseDto>>> getProductList(
+			@PageableDefault(size = 8, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+		return ApiResponse.success("상품 목록을 조회합니다.", productService.getProductList(pageable));
+	}
 
 	@Override
 	@GetMapping("/{productId}")
