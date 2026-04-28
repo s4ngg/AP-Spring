775c1f8003a22fadbac11a9cae784e01ec557eb4
Bora <sbr7518@gmail.com>
2026-04-28 16:05:49 +0900

    feat:상품 리스트 DTO 생성

--- src/main/java/co/kr/allpick/domain/product/repository/ProductRepository.java
+++ src/main/java/co/kr/allpick/domain/product/repository/ProductRepository.java
@@ -19,6 +19,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 		   "AND p.approvalStatus = 'APPROVED' ")
 	
 	Optional<Product> findValidProduct(@Param("id") Long productId);
+
+	
 }
 
   
