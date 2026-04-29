471ee3bf022746986ca5ebe8889fa931351013af
beanCan <bean.can0325@gmail.com>
2026-04-29 16:55:48 +0900

    feat: MemberService 인터페이스에 마이페이지 API 메서드 추가

--- src/main/java/co/kr/allpick/domain/member/service/MemberService.java
+++ src/main/java/co/kr/allpick/domain/member/service/MemberService.java
@@ -1,7 +1,21 @@
 package co.kr.allpick.domain.member.service;
 
 import co.kr.allpick.domain.member.dto.MemberResponseDto;
+import co.kr.allpick.domain.member.dto.MemberUpdateRequestDto;
+import co.kr.allpick.domain.member.dto.PasswordChangeRequestDto;
+import co.kr.allpick.domain.order.dto.OrderResponseDto;
+
+import java.util.List;
 
 public interface MemberService {
+
     MemberResponseDto getMember(Long memberId);
+
+    MemberResponseDto updateMember(Long memberId, MemberUpdateRequestDto request);
+
+    void changePassword(Long memberId, PasswordChangeRequestDto request);
+
+    void deleteMember(Long memberId);
+
+    List<OrderResponseDto> getMyOrders(Long memberId);
 }
