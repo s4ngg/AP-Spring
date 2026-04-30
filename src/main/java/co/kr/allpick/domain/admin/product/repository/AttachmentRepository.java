a1bfb47e8bc6cef045ec981c524cce1dbb044f20
Bora <sbr7518@gmail.com>
2026-04-30 14:51:05 +0900

    docs: Repository 주석 추가

--- src/main/java/co/kr/allpick/domain/admin/product/repository/AttachmentRepository.java
+++ src/main/java/co/kr/allpick/domain/admin/product/repository/AttachmentRepository.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
+    // 문의
     List<Attachment> findByInquiryIdAndDeletedAtIsNull(Long inquiryId);
 
+    // 교환/반품
     List<Attachment> findByClaimIdAndDeletedAtIsNull(Long claimId);
 }
