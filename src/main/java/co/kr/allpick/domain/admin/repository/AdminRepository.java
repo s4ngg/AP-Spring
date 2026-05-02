f4fd1b380b8de8da4ec01fc440d24baef6ece870
beanCan <bean.can0325@gmail.com>
2026-05-02 15:01:43 +0900

    fix: AdminRepository에 @Repository 어노테이션 추가

--- src/main/java/co/kr/allpick/domain/admin/repository/AdminRepository.java
+++ src/main/java/co/kr/allpick/domain/admin/repository/AdminRepository.java
@@ -2,9 +2,11 @@ package co.kr.allpick.domain.admin.repository;
 
 import co.kr.allpick.domain.admin.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     Optional<Admin> findByEmail(String email);
