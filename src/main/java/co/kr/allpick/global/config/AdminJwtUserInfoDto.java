03b9574b1c747f3119fcddca11cfe36263b6fb84
beanCan <bean.can0325@gmail.com>
2026-05-02 15:01:56 +0900

    fix: AdminJwtUserInfoDto에 @NoArgsConstructor, @Builder 어노테이션 추가

--- src/main/java/co/kr/allpick/global/config/AdminJwtUserInfoDto.java
+++ src/main/java/co/kr/allpick/global/config/AdminJwtUserInfoDto.java
@@ -2,10 +2,14 @@ package co.kr.allpick.global.config;
 
 import co.kr.allpick.domain.admin.entity.Admin;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AdminJwtUserInfoDto {
     private Long adminId;
     private String email;
