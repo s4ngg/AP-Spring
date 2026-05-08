5f8088145d40de3edce2212b120eb15c53ef9194
beanCan <bean.can0325@gmail.com>
2026-05-08 10:33:59 +0900

    fix: remove duplicate product image dto declarations

--- src/main/java/co/kr/allpick/domain/product/dto/ProductImageRequestDto.java
+++ src/main/java/co/kr/allpick/domain/product/dto/ProductImageRequestDto.java
@@ -15,9 +15,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-
-@Getter
-@NoArgsConstructor
 @Schema(description = "상품이미지 생성 요청dto")
 public class ProductImageRequestDto {
 	
@@ -35,7 +32,6 @@ public class ProductImageRequestDto {
 		return ProductImage.builder()
 				.productImageId(this.productImageId)
 				.imageUrl(this.imageUrl)
-				.product(product)
 				.sortOrder(this.sortOrder)
 				.product(product)
 				.build();
