cad4745bd153a7bb3cc10d253d6e64bef78cd5d1
piano <gungang1212@gmail.com>
2026-05-08 21:23:32 +0900

    fix: 어노테이션 수정

--- src/main/java/co/kr/allpick/domain/member/entity/Member.java
+++ src/main/java/co/kr/allpick/domain/member/entity/Member.java
@@ -26,7 +26,7 @@ public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "member_id")
     private Long id;
 
     @Column(nullable = false, unique = true, length = 100)
